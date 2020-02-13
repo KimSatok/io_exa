d2387cbb198333cd09797b383a8dad40c659cd47
Kim <kim.satokangas@gmail.com>
2020-02-13 13:45:06 +0200

    forms
    form code

---
+++ mypost.php
@@ -0,0 +1,20 @@
+<!DOCTYPE php>
+<html lang="en">
+
+<head>
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" type="text/css" href="css/mystyle.css" media="screen">
+    <title>mypost</title> 
+
+</head>
+
+<body>
+<?php print_r($_POST); ?>
+</body>
+
+    
+
+   
